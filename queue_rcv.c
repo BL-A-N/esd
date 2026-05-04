54d84b4f2fe0fb1900ecec91b67d0106977bb918
Anirudh R <140381948+BL-A-N@users.noreply.github.com>
2026-05-04 20:25:29 +0530

    Create queue_rcv.c

---
+++ queue_rcv.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#define max 512
+struct msg{
+long int msg_type;
+char some_text[max];};
+
+int main(){
+int running =1;
+int msgid;
+struct msg some_data;
+long int msg_to_rec=0;
+msgid=msgget((key_t)12345,0666|IPC_CREAT);
+while(running){
+msgrcv(msgid,(void*)&some_data,buf,msg,0);
+printf("Data received: %s\n",some_data.some_text);
+if(strncmp(some_data.some_text,"end",3)==0)
+{
+running=0;
+}
+}
+msgctl(msgid,IPC_RMID,0);
+}
