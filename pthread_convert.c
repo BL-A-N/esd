25684c535166cfc1b852d6d69de9b9f18e214681
Anirudh R <140381948+BL-A-N@users.noreply.github.com>
2026-05-05 18:25:08 +0530

    Update pthread_convert.c

--- pthread_convert.c
+++ pthread_convert.c
@@ -7,7 +7,7 @@
 #define buf 1024
 char buffer[buf];
 
-void *read (void *arg){
+void *read_t (void *arg){
   while(strncmp("stop",buffer,4)!=0)
   {
 printf("Enter text: ");
@@ -17,12 +17,12 @@ sleep(1);
 pthread_exit("read thread exit succesful");
 }
 
-void *convert (){
+void *convert_t (){
   int i;
 while (strncmp("stop",buffer,4)!=0){
 sleep(1);
 printf("Converted text: ");
-for(i=0,i<strlen(buffer),i++)
+for(i=0;i<strlen(buffer);i++)
   printf("%c",toupper(buffer[i]));
 }
 pthread_exit("convert thread exit successful");
@@ -33,12 +33,12 @@ int result;
 pthread_t rt,ct;
 void *thread_result;
 printf("Enter text, the program will convert it into upper case, \n To stop enter 'stop' \n");
-pthread_create(&rt,NULL,read,NULL);
-pthread_create(&ct,NULL,convert,NULL);
+pthread_create(&rt,NULL,read_t,NULL);
+pthread_create(&ct,NULL,convert_t,NULL);
 
 pthread_join(rt,&thread_result);
-printf("read thread joined, %s\n", (char)thread_result);
+printf("read thread joined, %s\n", (char *)thread_result);
 pthread_join(ct,&thread_result);
-printf("convert thread joined, %s\n", (char)thread_result);
+printf("convert thread joined, %s\n", (char *)thread_result);
 return (0);
 }
