aecf4043ddbd8ea2c0a2cd8f096458a27581eae7
Anirudh R <140381948+BL-A-N@users.noreply.github.com>
2026-06-09 21:46:02 +0530

    Update sin.c

--- sin.c
+++ sin.c
@@ -19,17 +19,16 @@ int main(void)
   uint16_t i;
   float angle;
 
-  PINSEL1 = 0x00080000;   // Enable DAC (P0.25)
+  PINSEL1 = 0x00080000;   
   IO0DIR  = 0xFFFFFFFF;
 
   while(1)
   {
     for(i = 0; i < 64; i++)
     {
-      angle = (2 * PI * i) / 64.0;              // angle in radians
-      value = (uint16_t)((sin(angle) * 511) + 512); // scale to 0–1023
-      DACR  = (value << 6);                     // align to DAC register
-
+      angle = (2 * PI * i) / 64.0;             
+      value = (uint16_t)((sin(angle) * 511) + 512); 
+      DACR  = (value << 6);                     
       delay_ms(1);
     }
   }
